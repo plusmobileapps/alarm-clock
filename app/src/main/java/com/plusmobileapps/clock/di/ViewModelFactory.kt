b9956d5484bd43be29cf32057f4ea62943109df7
Andrew Steinmetz <andrew@plusmobileapps.com>
2018-06-13 20:15:05 -0700

    clean up code

--- app/src/main/java/com/plusmobileapps/clock/di/ViewModelFactory.kt
+++ app/src/main/java/com/plusmobileapps/clock/di/ViewModelFactory.kt
@@ -3,7 +3,6 @@ package com.plusmobileapps.clock.di
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.plusmobileapps.clock.alarm.landing.AlarmLandingViewModel
-import com.plusmobileapps.clock.data.AlarmRepository
 import dagger.Binds
 import dagger.MapKey
 import dagger.Module
@@ -12,22 +11,6 @@ import dagger.multibindings.IntoMap
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.reflect.KClass
-
-////@Suppress("UNCHECKED_CAST")
-//@Singleton
-//class ViewModelFactory @Inject constructor(
-//        private val viewModels: MutableMap<Class<out ViewModel>, Provider<ViewModel>>): ViewModelProvider.Factory {
-//
-//
-//    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-//        return if (modelClass.isAssignableFrom(AlarmLandingViewModel::class.java)) {
-//            AlarmLandingViewModel(alarmRepository) as T
-//        } else {
-//            throw IllegalArgumentException("ViewModel Not Found")
-//        }
-//    }
-//}
-
 
 @Singleton
 class ViewModelFactory @Inject constructor(private val viewModels: MutableMap<Class<out ViewModel>, Provider<ViewModel>>) : ViewModelProvider.Factory {
